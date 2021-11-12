221d59e958a0a605e3b093f3e1131582f5b2bc35
Isaiah Mckenzie <Mr.isaiahmck@gmail.com>
2021-11-12 13:46:15 -0800

    Hot-Fixes

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ const poems = [
     image: "Zelda.jpeg",
   },
 
+  
   // YOUR CODE HERE: Add an object containing two key value pairs: author and poem. Set the author property as equal to a string with your first name. Set the poem property as equal to a short "roses are red" poem. Use the new line character as in the example when you would like a line break.
 ];
 
